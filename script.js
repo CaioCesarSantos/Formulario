0160ed782c0048e83755e5a0730cfd9904ee7672
Caio-Cesar <caiocesar9210@hotmail.com>
2023-07-29 19:31:44 -0300

    feat: Ultimos ajustes

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 const form = window.document.getElementById("form")
-const username =window.document.getElementById("username")
+const username = window.document.getElementById("username")
 const email = window.document.getElementById("email")
 const senha = window.document.getElementById("senha")
-const celular = window.document.getElementById("telefone")
+const celular = window.document.getElementById("celular")
+
 
 form.addEventListener("submit", (e) => {
     e.preventDefault();
@@ -10,48 +11,56 @@ form.addEventListener("submit", (e) => {
     itens();
 })
 
-function itens(){
-    const usernameValue= username.value;
-    const emailValue= email.value;
-    const senhaValue= senha.value;
-    const celularValue= celular.value;
 
-    if(usernameValue === ""){
-        setErrofor(username , "O nome do usuário é obrigatorio");
+function itens() {
+    const usernameValue = username.value;
+    const emailValue = email.value;
+    const senhaValue = senha.value;
+    const celularValue = celular.value;
+
+    if (usernameValue === "") {
+        setErrorfor(username, "O nome do usuásrio é obrigatorio");
     }else{
         steSucess(username)
     }
 
     if(emailValue === ""){
-        setErrofor(email , "O email é obrigatorio");
+        setErrorfor(email , "O email é Obrigatório")
     }else{
         steSucess(email)
-    }
+    } 
 
-    
-    if(senhaValue === ""){
-        setErrofor(senha , " A senha é obrigatorio");
+     if(senhaValue === ""){
+        setErrorfor(senha , "Senha é Obrigatória")
     }else{
         steSucess(senha)
     }
 
     if(celularValue === ""){
-        setErrofor(celular , "O telefone é obrigatorio");
+        setErrorfor(celular, "Número de telefone é Obrigatório")
     }else{
         steSucess(celular)
     }
 
+
+
 }
 
-function setErrofor(input , messagem){
+function setErrorfor(input, messagem) {
+
     const caixaform = input.parentElement;
     const small = caixaform.querySelector("small")
 
-    small.innerText= messagem;
+    small.innerText = messagem;
+
     caixaform.className = "caixa-form error"
 }
 
-function steSucess(input){
+function steSucess(input) {
+
     const caixaform = input.parentElement;
+
+
     caixaform.className = "caixa-form sucess"
 }
+
